118b85ad9a61e7cfe67ae6bf8bc70ffb691d7018
gabriellaeun <68334229+gabriellaeun@users.noreply.github.com>
2021-06-03 11:12:56 +0900

    Update Lower.cs
    상악 하악 따로 제어(child로 넣지 않음)
    -> 지대치는 하악의 child로 넣었음

--- Lower.cs
+++ Lower.cs
@@ -7,7 +7,8 @@ public class Lower : MonoBehaviour
     // 하악은 상악의 child로 들어가있기 때문에 상악의 위치로부터 바뀌는 위치 표시
     void Start()
     {
-        transform.localPosition = new Vector3(0f, 0f, -4f);
-        transform.Rotate(0, 0, 20);
+        transform.localPosition = new Vector3(-0.002f, -6.2f, 80f);
+        transform.rotation = Quaternion.Euler(new Vector3(-90f, -10f, 0f));
+        transform.localScale = new Vector3(1f, 1f, 1f);
     }
 }
