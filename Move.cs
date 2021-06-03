77ee4606188e501f9f409a1311d25a92887165f9
gabriellaeun <68334229+gabriellaeun@users.noreply.github.com>
2021-06-03 11:13:33 +0900

    Update Move.cs

--- Move.cs
+++ Move.cs
@@ -6,7 +6,8 @@ public class Move : MonoBehaviour
 {
     void OnMouseDrag()
     {
-        Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, transform.position.z);
+        //오브젝트 z position은 80으로 고정
+        Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 80);
         Vector3 objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
         transform.position = objPosition;
     }
