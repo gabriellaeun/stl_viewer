8231c30123e01d60448d8ecc6b6233409c11f4a8
gabriellaeun <68334229+gabriellaeun@users.noreply.github.com>
2021-06-17 09:29:24 +0900

    Update InfoText.cs

--- InfoText.cs
+++ InfoText.cs
@@ -29,13 +29,27 @@ public class InfoText : MonoBehaviour
         */
 
         /*
+        //파일 경로에서 .constructionInfo 파일이면  xml로 바꾸고 xml이면 그대로 가는 것으로 코드 짜기
+
+
         string FileName = "C:\\Users\\82105\\stl_viewer\\Assets\\Resources\\2018-06-08_162694979.constructionInfo";
-        string result = Path.ChangeExtension(FileName, ".xml"); //.constructionInfo 파일의 확장자를 .xml로 변환
-        File.Move(FileName, result);
-        Debug.Log(result);
-        string NAME = Path.GetFileNameWithoutExtension(result);//확장자를 뺀 파일의 이름
-        TextAsset textAsset = (TextAsset)Resources.Load(NAME);
-        Debug.Log(textAsset);
+
+        if(strcmp(Path.GetExtension(FileName), ".constructionInfo") == 0){
+            string result = Path.ChangeExtension(FileName, ".xml"); //.constructionInfo 파일의 확장자를 .xml로 변환
+            File.Move(FileName, result);
+            Debug.Log(result);
+            string NAME = Path.GetFileNameWithoutExtension(result);//확장자를 뺀 파일의 이름
+            TextAsset textAsset = (TextAsset)Resources.Load(NAME);
+            Debug.Log(textAsset);   
+        }
+        else if(strcmp(Path.GetExtension(FileName), ".xml") == 0){
+            string NAME = Path.GetFileNameWithoutExtension(FileName);
+        }
+        else{
+            Debug.Log("path extension error");
+        }
+
+        XmlReader reader = XmlReader.Create(FileName);
         */
         string xmlPath = "C:\\Users\\82105\\stl_viewer\\Assets\\Resources\\2018-06-08_162694979.xml";
         string NAME = Path.GetFileNameWithoutExtension(xmlPath);
@@ -49,25 +63,35 @@ public class InfoText : MonoBehaviour
                 switch (reader.Name.ToString())
                 {
                     case "Number":
-                        Debug.Log("Number : " + reader.ReadString());
-                        Number.text = "Number : " + reader.ReadString();
-                        break;
+                        {
+                            Debug.Log("Number : " + reader.ReadString());
+                            Number.text = "Number : " + reader.ReadString();
+                            break;
+                        }
                     case "ReconstructionType":
-                        Debug.Log("Type : " + reader.ReadString());
-                        ReconstructionType.text = "Type : " + reader.ReadString();
-                        break;
+                        {
+                            Debug.Log("Type : " + reader.ReadString());
+                            ReconstructionType.text = "Type : " + reader.ReadString();
+                            break;
+                        }
                     case "ToothLibraryName":
-                        Debug.Log("Tooth Library Name : " + reader.ReadString());
-                        ToothLibraryName.text = "Tooth Library Name : " + reader.ReadString();
-                        break;
+                        {
+                            Debug.Log("Tooth Library Name : " + reader.ReadString());
+                            ToothLibraryName.text = "Tooth Library Name : " + reader.ReadString();
+                            break;
+                        }
                     case "ToothModelFilename":
-                        Debug.Log("Tooth Library File : " + reader.ReadString());
-                        ToothModelFilename.text = "Tooth Library File : " + reader.ReadString();
-                        break;
+                        {
+                            Debug.Log("Tooth Library File : " + reader.ReadString());
+                            ToothModelFilename.text = "Tooth Library File : " + reader.ReadString();
+                            break;
+                        }
                     case "ImplantType":
-                        Debug.Log("Implant Type : " + reader.ReadString());
-                        ImplantType.text = "Implant Type : " + reader.ReadString();
-                        break;
+                        {
+                            Debug.Log("Implant Type : " + reader.ReadString());
+                            ImplantType.text = "Implant Type : " + reader.ReadString();
+                            break;
+                        }
                 }
             }
         }
