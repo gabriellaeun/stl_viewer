0b9491c11e5b9a98922c074621b157ed09d34aea
gabriellaeun <68334229+gabriellaeun@users.noreply.github.com>
2021-06-24 12:06:44 +0900

    Update FolderDialog.cs
    선택한 폴더 내의 파일 경로 출력 코드

--- FolderDialog.cs
+++ FolderDialog.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Windows.Forms;
+using System.IO;
 
 public class FileDialog : MonoBehaviour
 {
@@ -12,6 +13,12 @@ public class FileDialog : MonoBehaviour
         dialog.ShowDialog();
         string select_path = dialog.SelectedPath;
         Debug.Log("선택된 폴더" + select_path);
+
+        DirectoryInfo di = new DirectoryInfo(@select_path);
+        foreach(FileInfo file in di.GetFiles())
+        {
+            Debug.Log("파일명 : " + file.Name);
+        }
     }
 
 }
