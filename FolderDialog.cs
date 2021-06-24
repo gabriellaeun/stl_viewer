0eac5d664a19ec5f7dbfa0de950f7006148a12de
gabriellaeun <68334229+gabriellaeun@users.noreply.github.com>
2021-06-24 17:48:11 +0900

    Update FolderDialog.cs

--- FolderDialog.cs
+++ FolderDialog.cs
@@ -6,15 +6,16 @@ using System.IO;
 
 public class FileDialog : MonoBehaviour
 {
-       
     public void FileOpen()
     {
         FolderBrowserDialog dialog = new FolderBrowserDialog();
         dialog.ShowDialog();
-        string select_path = dialog.SelectedPath;
-        Debug.Log("선택된 폴더" + select_path);
+        string select_path = @dialog.SelectedPath;
+        Debug.Log("선택된 폴더 : " + select_path);
+        string DirName = Path.GetFileName(Path.GetDirectoryName(select_path));
+        Debug.Log("폴더 이름 : " + DirName);
 
-        DirectoryInfo di = new DirectoryInfo(@select_path);
+        DirectoryInfo di = new DirectoryInfo(select_path);
         foreach(FileInfo file in di.GetFiles())
         {
             Debug.Log("파일명 : " + file.Name);
@@ -33,8 +34,28 @@ public class FileDialog : MonoBehaviour
         string proj_path = UnityEngine.Application.dataPath; //1. 프로젝트 Assets 경로
         Debug.Log(proj_path);
 
-        //2.select_path 폴더를 현 프로젝트의 Asset으로 옮기는 코드
+        string target_path = @Path.Combine(proj_path, "File");//디렉토리 이름이 File이 아니고 사실 복사하는 디렉토리의 이름과 같아야 함
 
+        Directory.CreateDirectory(target_path);
+
+        //2.select_path 폴더를 현 프로젝트의 Asset으로 옮기는 코드
+        //File 디렉토리는 생기는데 폴더 내의 파일이 옮겨지지가 않음     
+        if (Directory.Exists(select_path))
+        {
+            string[] files = Directory.GetFiles(select_path);
+            foreach(string s in files)
+            {
+                string fileName = Path.GetFileName(s);
+                string destFile = Path.Combine(target_path, fileName);
+                File.Copy(s, destFile, true);
+            }
+        }
+        else
+        {
+            Debug.Log("Source path does not exist");
+        }
     }
+
+    
 
 }
