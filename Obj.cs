6cee530e922b82f00d610f49a227144bad280845
gabriellaeun <68334229+gabriellaeun@users.noreply.github.com>
2021-08-24 11:05:25 +0900

    Update Obj.cs

--- Obj.cs
+++ Obj.cs
@@ -9,6 +9,13 @@ public class Obj : MonoBehaviour
     public GameObject targetU;
     public GameObject Abut;
 
+    Toggle toggleU;
+    Toggle toggleL;
+    Toggle toggleA;
+    start initU;
+    start initL;
+    start initA;
+
     public void GetObj()
     {
         FileDialog name = GameObject.Find("folder").GetComponent<FileDialog>();
@@ -27,21 +34,62 @@ public class Obj : MonoBehaviour
                 if (targetU == null) //상악 이름이 1-COPY인 경우
                 {
                     targetU = Resources.Load<GameObject>(name.DirName + "/1-COPY");
-                    Instantiate(targetL, new Vector3(0.05f, -0.2f, 80f), Quaternion.Euler(new Vector3(-90f, -10f, 0f))); //하악
+                    GameObject cloneL = Instantiate(targetL, new Vector3(0.05f, -0.2f, 80f), Quaternion.Euler(new Vector3(-90f, -10f, 0f))); //하악
                     targetL.transform.localScale = new Vector3(1f, 1f, 1f);
-                    Instantiate(targetU, new Vector3(0.05f, -0.2f, 80f), Quaternion.Euler(new Vector3(-90f, -10f, 0f))); //상악
+                    GameObject cloneU = Instantiate(targetU, new Vector3(0.05f, -0.2f, 80f), Quaternion.Euler(new Vector3(-90f, -10f, 0f))); //상악
                     targetU.transform.localScale = new Vector3(1f, 1f, 1f);
-                    Instantiate(Abut, new Vector3(0.05f, -0.2f, 80f), Quaternion.Euler(new Vector3(-90f, -10f, 0f))); //상악 지대치
+                    GameObject cloneA = Instantiate(Abut, new Vector3(0.05f, -0.2f, 80f), Quaternion.Euler(new Vector3(-90f, -10f, 0f))); //상악 지대치
                     Abut.transform.localScale = new Vector3(1f, 1f, 1f);
+
+                    cloneL.AddComponent<Rotate>();
+                    cloneL.AddComponent<Zoom>();
+                    cloneU.AddComponent<Rotate>();
+                    cloneU.AddComponent<Zoom>();
+                    cloneA.AddComponent<Rotate>();
+                    cloneA.AddComponent<Zoom>();
+
+                    toggleU = GameObject.Find("UpperTog").GetComponent<Toggle>();
+                    toggleU.gameobj = cloneU;
+                    toggleL = GameObject.Find("LowerTog").GetComponent<Toggle>();
+                    toggleL.gameobj = cloneL;
+                    toggleA = GameObject.Find("AbutmentTog").GetComponent<Toggle>();
+                    toggleA.gameobj = cloneA;
+                    initU = GameObject.Find("initialize").GetComponent<start>();
+                    initU.upper = cloneU;
+                    initL = GameObject.Find("initialize").GetComponent<start>();
+                    initL.lower = cloneL;
+                    initA = GameObject.Find("initialize").GetComponent<start>();
+                    initA.abut = cloneA;
                 }
                 else //상악 이름이 1-copy인 경우
                 {
-                    Instantiate(targetL, new Vector3(0.05f, -0.2f, 80f), Quaternion.Euler(new Vector3(-90f, -10f, 0f))); //하악
+                    GameObject cloneL = Instantiate(targetL, new Vector3(0.05f, -0.2f, 80f), Quaternion.Euler(new Vector3(-90f, -10f, 0f))); //하악
                     targetL.transform.localScale = new Vector3(1f, 1f, 1f);
-                    Instantiate(targetU, new Vector3(0.05f, -0.2f, 80f), Quaternion.Euler(new Vector3(-90f, -10f, 0f))); //상악
+                    GameObject cloneU = Instantiate(targetU, new Vector3(0.05f, -0.2f, 80f), Quaternion.Euler(new Vector3(-90f, -10f, 0f))); //상악
                     targetU.transform.localScale = new Vector3(1f, 1f, 1f);
-                    Instantiate(Abut, new Vector3(0.05f, -0.2f, 80f), Quaternion.Euler(new Vector3(-90f, -10f, 0f))); //상악 지대치
+                    GameObject cloneA = Instantiate(Abut, new Vector3(0.05f, -0.2f, 80f), Quaternion.Euler(new Vector3(-90f, -10f, 0f))); //상악 지대치
                     Abut.transform.localScale = new Vector3(1f, 1f, 1f);
+
+                    cloneL.AddComponent<Rotate>();
+                    cloneL.AddComponent<Zoom>();
+                    cloneU.AddComponent<Rotate>();
+                    cloneU.AddComponent<Zoom>();
+                    cloneA.AddComponent<Rotate>();
+                    cloneA.AddComponent<Zoom>();
+
+                    toggleU = GameObject.Find("UpperTog").GetComponent<Toggle>();
+                    toggleU.gameobj = cloneU;
+                    toggleL = GameObject.Find("LowerTog").GetComponent<Toggle>();
+                    toggleL.gameobj = cloneL;
+                    toggleA = GameObject.Find("AbutmentTog").GetComponent<Toggle>();
+                    toggleA.gameobj = cloneA;
+
+                    initU = GameObject.Find("initialize").GetComponent<start>();
+                    initU.upper = cloneU;
+                    initL = GameObject.Find("initialize").GetComponent<start>();
+                    initL.lower = cloneL;
+                    initA = GameObject.Find("initialize").GetComponent<start>();
+                    initA.abut = cloneA;
                 }
             }
             else //하악에 지대치가 있을 경우(하악 이름이 2-COPY(COPY 대문자로 되어있는 경우))
@@ -49,12 +97,33 @@ public class Obj : MonoBehaviour
                 targetL = Resources.Load<GameObject>(name.DirName + "/2-COPY");
                 targetU = Resources.Load<GameObject>(name.DirName + "/1");
                 Abut = Resources.Load<GameObject>(name.DirName + "/2");
-                Instantiate(targetL, new Vector3(0.05f, -0.2f, 80f), Quaternion.Euler(new Vector3(-90f, -10f, 0f))); //하악
+                GameObject cloneL = Instantiate(targetL, new Vector3(0.05f, -0.2f, 80f), Quaternion.Euler(new Vector3(-90f, -10f, 0f))); //하악
                 targetL.transform.localScale = new Vector3(1f, 1f, 1f);
-                Instantiate(targetU, new Vector3(0.05f, -0.2f, 80f), Quaternion.Euler(new Vector3(-90f, -10f, 0f))); //상악
+                GameObject cloneU = Instantiate(targetU, new Vector3(0.05f, -0.2f, 80f), Quaternion.Euler(new Vector3(-90f, -10f, 0f))); //상악
                 targetU.transform.localScale = new Vector3(1f, 1f, 1f);
-                Instantiate(Abut, new Vector3(0.05f, -0.2f, 80f), Quaternion.Euler(new Vector3(-90f, -10f, 0f))); //하악 지대치
+                GameObject cloneA = Instantiate(Abut, new Vector3(0.05f, -0.2f, 80f), Quaternion.Euler(new Vector3(-90f, -10f, 0f))); //하악 지대치
                 Abut.transform.localScale = new Vector3(1f, 1f, 1f);
+
+                cloneL.AddComponent<Rotate>();
+                cloneL.AddComponent<Zoom>();
+                cloneU.AddComponent<Rotate>();
+                cloneU.AddComponent<Zoom>();
+                cloneA.AddComponent<Rotate>();
+                cloneA.AddComponent<Zoom>();
+
+                toggleU = GameObject.Find("UpperTog").GetComponent<Toggle>();
+                toggleU.gameobj = cloneU;
+                toggleL = GameObject.Find("LowerTog").GetComponent<Toggle>();
+                toggleL.gameobj = cloneL;
+                toggleA = GameObject.Find("AbutmentTog").GetComponent<Toggle>();
+                toggleA.gameobj = cloneA;
+
+                initU = GameObject.Find("initialize").GetComponent<start>();
+                initU.upper = cloneU;
+                initL = GameObject.Find("initialize").GetComponent<start>();
+                initL.lower = cloneL;
+                initA = GameObject.Find("initialize").GetComponent<start>();
+                initA.abut = cloneA;
             }
         }
         else //하악에 지대치가 있을 경우(하악 이름이 2-copy)
@@ -62,12 +131,33 @@ public class Obj : MonoBehaviour
             targetL = Resources.Load<GameObject>(name.DirName + "/2-copy");
             targetU = Resources.Load<GameObject>(name.DirName + "/1");
             Abut = Resources.Load<GameObject>(name.DirName + "/2");
-            Instantiate(targetL, new Vector3(0.05f, -0.2f, 80f), Quaternion.Euler(new Vector3(-90f, -10f, 0f))); //하악
+            GameObject cloneL = Instantiate(targetL, new Vector3(0.05f, -0.2f, 80f), Quaternion.Euler(new Vector3(-90f, -10f, 0f))); //하악
             targetL.transform.localScale = new Vector3(1f, 1f, 1f);
-            Instantiate(targetU, new Vector3(0.05f, -0.2f, 80f), Quaternion.Euler(new Vector3(-90f, -10f, 0f))); //상악
+            GameObject cloneU = Instantiate(targetU, new Vector3(0.05f, -0.2f, 80f), Quaternion.Euler(new Vector3(-90f, -10f, 0f))); //상악
             targetU.transform.localScale = new Vector3(1f, 1f, 1f);
-            Instantiate(Abut, new Vector3(0.05f, -0.2f, 80f), Quaternion.Euler(new Vector3(-90f, -10f, 0f))); //하악 지대치
+            GameObject cloneA = Instantiate(Abut, new Vector3(0.05f, -0.2f, 80f), Quaternion.Euler(new Vector3(-90f, -10f, 0f))); //하악 지대치
             Abut.transform.localScale = new Vector3(1f, 1f, 1f);
+
+            cloneL.AddComponent<Rotate>();
+            cloneL.AddComponent<Zoom>();
+            cloneU.AddComponent<Rotate>();
+            cloneU.AddComponent<Zoom>();
+            cloneA.AddComponent<Rotate>();
+            cloneA.AddComponent<Zoom>();
+
+            toggleU = GameObject.Find("UpperTog").GetComponent<Toggle>();
+            toggleU.gameobj = cloneU;
+            toggleL = GameObject.Find("LowerTog").GetComponent<Toggle>();
+            toggleL.gameobj = cloneL;
+            toggleA = GameObject.Find("AbutmentTog").GetComponent<Toggle>();
+            toggleA.gameobj = cloneA;
+
+            initU = GameObject.Find("initialize").GetComponent<start>();
+            initU.upper = cloneU;
+            initL = GameObject.Find("initialize").GetComponent<start>();
+            initL.lower = cloneL;
+            initA = GameObject.Find("initialize").GetComponent<start>();
+            initA.abut = cloneA;
         }
 
     }
