710bc326263f9f1b31d60948863d8293303be36d
gabriellaeun <68334229+gabriellaeun@users.noreply.github.com>
2021-09-11 23:51:35 +0900

    Update TimeManager.cs

--- TimeManager.cs
+++ TimeManager.cs
@@ -9,6 +9,7 @@ public class TimeManager : MonoBehaviour
     public Text Date;
     public string CurrentTime;
     public string buildDate;
+    public string xmlName;
 
     void Start()
     {
@@ -17,6 +18,7 @@ public class TimeManager : MonoBehaviour
         Date.text = DateTime.Now.ToString(("yyyy-MM-dd HH:mm:ss tt"));
         buildDate = DateTime.Now.ToString(("yyyy-MM-dd"));
         CurrentTime = Date.text;
+        xmlName = DateTime.Now.ToString(("yyyy-MM-dd-HHmmss"));
         //Debug.Log(CurrentTime);
     }
 }
