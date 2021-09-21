079da4033f4fc8e8113aca6cdd5375c0d08e5b80
gabriellaeun <68334229+gabriellaeun@users.noreply.github.com>
2021-09-21 22:07:08 +0900

    Update InputFieldSave.cs
    position xml 저장

--- InputFieldSave.cs
+++ InputFieldSave.cs
@@ -2,15 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.IO;
+using System.Xml;
 
+/*
+ * 거래처 이름 저장하는 스크립트
+ */
 public class InputFieldSave : MonoBehaviour
 {
     public InputField inputField;
     public string fieldText;
+    DataSave dataSave;
 
-    public void InputFieldSave()
+    public void Save()
     {
-        fieldText = inputField.text;
+        dataSave = GameObject.Find("DataSave").GetComponent<DataSave>();
+
+        string filename = dataSave.name;
+        Debug.Log(filename);
+
+        TextAsset textAsset = (TextAsset)Resources.Load("Data/" + filename); //xml 파일 로드
+        Debug.Log(textAsset);
+        XmlDocument xmlDoc = new XmlDocument();
+        xmlDoc.LoadXml(textAsset.text);
+
+        XmlNodeList nodes = xmlDoc.SelectNodes("Treatment/Practice");
+        XmlNode Position = nodes[0];
+
+        Position.SelectSingleNode("PracticeName").InnerText = inputField.text;
+        Debug.Log(inputField.text);
+        xmlDoc.Save("Assets/Resources/Data/" + filename + ".xml");
     }
 
 }
