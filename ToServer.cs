c23dccbcc89b9ed0e07f513bcdd728da553aea48
gabriellaeun <68334229+gabriellaeun@users.noreply.github.com>
2021-12-29 20:11:19 +0900

    Update ToServer.cs

--- ToServer.cs
+++ ToServer.cs
@@ -3,20 +3,52 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 using System.IO;
-using Newtonsoft.Json;
 using System.Text;
+using LitJson;
 
 public class ToServer : MonoBehaviour
 {
-    
+    public static string margin;
+    public static string marginURL;
+    public static string marginPoint;
+    int result;
+    string txt;
+
     public void MarginExtract()
     {
         StartCoroutine(DataPost());
     }
 
+    private void FirstParser(string jsonString)
+    {
+        JsonData jsonParser = JsonMapper.ToObject(jsonString);
+        margin = jsonParser["margin id"].ToString();
+        marginURL = jsonParser["check URL"].ToString();
+
+        Debug.Log("margin : " + margin);
+        Debug.Log("URL : " + marginURL);
+    }
+
+    private int SecondParser(string jString)
+    {
+        JsonData Parser = JsonMapper.ToObject(jString);
+
+        string margin2 = Parser["id"].ToString();
+        marginPoint = Parser["margin"].ToString();
+
+        Debug.Log("margin : " + margin2);
+        Debug.Log("Point : " + marginPoint);
+
+        if (marginPoint == "")
+            return 1;
+        else
+            return 2;
+    }
+
     IEnumerator DataPost()
     {
         string url = "http://dentalcad.ai:6003/margin/req_nonblock/";
+        UnityWebRequest www;
 
         byte[] bytes = File.ReadAllBytes(@"C:\Users\82105\Desktop\ClearTech\다른치아_샘플\2017-11-14_100217566\2.stl");
 
@@ -26,36 +58,96 @@ public class ToServer : MonoBehaviour
         form.AddBinaryData("file", bytes, @"C:\Users\82105\Desktop\ClearTech\다른치아_샘플\2017-11-14_100217566\2.stl", "application/octet-stream");
 
 
-        UnityWebRequest www = UnityWebRequest.Post(url, form);
-        //www.downloadHander = new DownloadHandlerBuffer();
-        //www.SetRequestHeader("Content-Type", "application/json");
+        www = UnityWebRequest.Post(url, form);
 
         yield return www.SendWebRequest();
-
+        
         if (www.result != UnityWebRequest.Result.Success)
         {
             Debug.Log(www.error);
         }
         else
         {
-            Debug.Log("성공");
-            /*
-            JObject decoded = JObject.Parse(www.downloadHandler.text);
+            Debug.Log(www.downloadHandler.text);
+            FirstParser(www.downloadHandler.text);
 
-            JToken datas = decoded["arrayKey"];
-            foreach(JToken data in datas)
-            {
-                Debug.Log(data["key1"].ToObject<string>());
-                Debug.Log(data["key2"].ToObject<string>());
-            }
-            */
-            //Debug.LogFormat("{0}\n{1}\n{2}", www.responseCode, www.downloadHandler.data, www.downloadHandler.text);
-            
+            StartCoroutine(DataGet());
         }
 
 
     }
-    
+
+    IEnumerator DataGet()
+    {
+        UnityWebRequest get;
+        
+        get = UnityWebRequest.Get(marginURL);
+
+        yield return get.SendWebRequest();
+
+        if (get.result != UnityWebRequest.Result.Success)
+        {
+            Debug.Log(get.error);
+        }
+        else
+        {
+            Debug.Log(marginURL + " : Get하는 중");
+
+            //get.downloadHander = new DownloadHandlerBuffer();
+            
+            
+            txt = get.downloadHandler.text.Replace("&quot;", "\"");
+            Debug.Log(txt);
+            result = SecondParser(txt);
+
+            
+            while (result == 1)
+            {
+                txt = null;
+                get = UnityWebRequest.Get(marginURL);
+
+                yield return get.SendWebRequest();
+
+                if (get.result != UnityWebRequest.Result.Success)
+                {
+                    Debug.Log(get.error);
+                }
+                else
+                {
+                    Debug.Log(marginURL + " : Get하는 중");
+
+                    //get.downloadHander = new DownloadHandlerBuffer();
+                    txt = get.downloadHandler.text.Replace("&quot;", "\"");
+                    Debug.Log(txt);
+                    result = SecondParser(txt);
+
+
+                }
+            }
+
+            
+
+
+            Debug.Log("마진 추출 완료!");
+            /*
+            if(result == 1)
+            {
+                Debug.Log("마진 추출 전");
+
+            }
+            */
+
+            //File.WriteAllBytes(@"C:\Users\File", get.downloadHander.text);
+
+            string path = DataSave.target_path;
+
+            string marginPath = path + "/" + DataSave.dirName + "-" + margin + ".xyz";
+
+            File.WriteAllText(@marginPath, txt, Encoding.Default);
+        }
+        
+    }
+
 }
 
 
